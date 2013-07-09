6be65fbf6bc31e1264f18a79af737f76bbb6a918
vasudhashabad <vasudha.s@nucleusgroupcompanies.com>
2013-07-09 17:20:32 +0530

    title msg

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -8,7 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>
             @section('title')
-            Laravel Radiate
+            Asterix 
             @show
         </title>
         <meta name="description" content="">
