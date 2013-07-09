07fbedcb25d2660d40711c4f57209cae20ba06e0
chinnareddy <praveenreddy422@gmail.com>
2013-07-09 12:19:04 +0530

    commited by praveenreddy

--- reg.php
+++ reg.php
@@ -5,7 +5,8 @@ New file
 </head>
 <body>
 <form action="" method="post">
-		Name:<input type="text" name="uname">{{$errors->first('uname', '<span class="error">:message</span>')}}<br/>
+		FName:<input type="text" name="uname">{{$errors->first('uname', '<span class="error">:message</span>')}}<br/>
+        LName:<input type="text" name="uname">{{$errors->first('uname', '<span class="error">:message</span>')}}<br/>
 		Email: <input type="email" name="email">{{ $errors->first('email','<span class="error">:message</span>')}}<br/>
 		Password: <input type="password" name="password">{{ $errors->first('password','<span class="error">:message</span>')}}<br/>
 		<input type="submit" name="signup" value="submit" >
