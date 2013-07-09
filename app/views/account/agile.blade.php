42ac5b67c38940642c61df27978f849a10939d38
vasudhashabad <vasudha.s@nucleusgroupcompanies.com>
2013-07-09 17:10:52 +0530

    commit msg

---
+++ app/views/account/agile.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+
+{{-- Web site Title --}}
+@section('title')
+@parent
+:: Products
+@stop
+
+{{-- Content --}}
+@section('content')
+<div class="page-header">
+	<h1> AGILE</h1>
+	<p></p>	
+</div>
+
+
+@stop
