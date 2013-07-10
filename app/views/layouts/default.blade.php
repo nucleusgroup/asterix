c22c40b56a4cc6d3fdb5db681e92e7bab7364e4c
gnarendarreddy <narendarreddy.g@nucleusgroupcompanies.com>
2013-07-10 12:25:45 +0530

    Update default.blade.php
    nothing to change

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -83,7 +83,7 @@
             <!-- ./ content -->
         </div>
         <!-- ./ container -->
-
+              @yield('container')
         <!-- jQuery -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="assets/scripts/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
