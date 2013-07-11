b8e73d5c5c01603f2cae5d282aefa56ad916e73e
vasudhashabad <vasudha.s@nucleusgroupcompanies.com>
2013-07-11 17:59:03 +0530

    changes to app/views/account/home1.blade.php

--- app/views/account/home1.blade.php
+++ app/views/account/home1.blade.php
@@ -31,28 +31,38 @@
 
 		<div class="span9">
             <div class="hero-unit">
- 			    <p>PLAYJECT is an Experiential project management Learning and development platform that is Uniquely designed to appreciate the dynamic, complex, and unique nature of projects.
+ 			    <p style="color:black">PLAYJECT is an Experiential project management Learning and development platform that is Uniquely designed to appreciate the dynamic, complex, and unique nature of projects.
                 </p>
-                <p>PLAYJECT combines the core elements of project management body of knowledge and global best practices in Project Management as espoused by PMI (Project Management Institute) with a game based experiential learning to enhance project management skills.
+                <p style="color:black">PLAYJECT combines the core elements of project management body of knowledge and global best practices in Project Management as espoused by PMI (Project Management Institute) with a game based experiential learning to enhance project management skills.
                 </p>
                 
             </div>
             <div class="row-fluid">
+                <div class="span6">
+                    <div class="main_area_video_content" id="main_content">
+                        <div id="videotext">
+                            <iframe width="320" height="300" src="http://www.youtube.com/embed/ZDR433b0HJY?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="span3">
+                    <p>PLAYJECT combines the core elements of project management body of knowledge and global best practices in Project Management as espoused by PMI (Project Management Institute) with a game based experiential learning to enhance project management skills.
+                    </p>
+                </div>
+            </div>
 
+            <div class="btn_right">
+                <!--<input name="pre2" value="View document" class="btn btn_right_btn" type="button" />-->
+                <input name="pre" value="Hide text" class="btnright2 btn_right_btn" type="button" id="showhide" onClick="showhidetext()" />
             </div>
 		</div>
-		<div class="btn_right">
-            <!--<input name="pre2" value="View document" class="btn btn_right_btn" type="button" />-->
-            <input name="pre" value="Hide text" class="btnright2 btn_right_btn" type="button" id="showhide" onClick="showhidetext()" />
-          
-        </div>
-      </div>
-      <div class="main_area_video_content" id="main_content">
-        <div id="videotext"></div>
-      </div>
-	</div>
+		
+    </div>
+      
 </div>
+    
 </div>
+
 <script type="text/javascript">
 
  function showhidetext() {
