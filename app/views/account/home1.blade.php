dc029f961a85012697bc383da111a46d8d7634e3
jyothib12 <jyothib12@gmail.com>
2013-07-11 18:18:00 +0530

    cc

--- app/views/account/home1.blade.php
+++ app/views/account/home1.blade.php
@@ -33,13 +33,12 @@
 
             <div class="hero-unit">
  			    <p style="color:black">PLAYJECT is an Experiential project management Learning and development platform that is Uniquely designed to appreciate the dynamic, complex, and unique nature of projects.
-
                 </p>
                 <p style="color:black">PLAYJECT combines the core elements of project management body of knowledge and global best practices in Project Management as espoused by PMI (Project Management Institute) with a game based experiential learning to enhance project management skills.
                 </p>
         
             </div>
-
+            <div class="row-fluid">
                 <div class="span6">
                     <div class="main_area_video_content" id="main_content">
                         <div id="videotext">
@@ -51,16 +50,16 @@
                     <p>PLAYJECT combines the core elements of project management body of knowledge and global best practices in Project Management as espoused by PMI (Project Management Institute) with a game based experiential learning to enhance project management skills.
                     </p>
                 </div>
+            
+
+                <div class="btn_right" align="center">
+                    <!--<input name="pre2" value="View document" class="btn btn_right_btn" type="button" />-->
+                    <input name="pre" value="Hide text" class="btnright2 btn_right_btn" type="button" id="showhide" onClick="showhidetext()" />
+
+                </div>	
             </div>
-
-            <div class="btn_right" align="center">
-                <!--<input name="pre2" value="View document" class="btn btn_right_btn" type="button" />-->
-                <input name="pre" value="Hide text" class="btnright2 btn_right_btn" type="button" id="showhide" onClick="showhidetext()" />
-
-            </div>	
-        </div>
       
-    </div>
+        </div>
     </div>
 </div>
 
